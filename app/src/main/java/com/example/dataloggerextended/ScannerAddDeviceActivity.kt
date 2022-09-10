252cb43effe0fbc15ad2d34e2ef34bdc62d7dce8
DanielRoffo <daniroffo125@gmail.com>
2022-09-10 15:44:10 -0300

    Scanner ya reconoce los Qr de los dispositivos

--- app/src/main/java/com/example/dataloggerextended/ScannerAddDeviceActivity.kt
+++ app/src/main/java/com/example/dataloggerextended/ScannerAddDeviceActivity.kt
@@ -1,9 +1,13 @@
 package com.example.dataloggerextended
 
 import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,9 +19,17 @@ import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
 import com.example.dataloggerextended.databinding.ActivityDevicesBinding
 import com.example.dataloggerextended.databinding.ActivityScannerAddDeviceBinding
+import com.example.dataloggerextended.ui.view.AddDeviceActivity
+import com.example.dataloggerextended.ui.view.DevicesActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ScannerAddDeviceActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val dbDevices = Firebase.firestore.collection("devices")
+    private val dbDevicesUsers = Firebase.firestore.collection("users")
     private lateinit var codeScanner: CodeScanner
     private lateinit var binding: ActivityScannerAddDeviceBinding
 
@@ -25,11 +37,21 @@ class ScannerAddDeviceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanner_add_device)
 
+        // Initialize Firebase Auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Cambiar el color del SupportActionBar
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.light_black)))
+        supportActionBar!!.title = "Scan new device"
+
+        //Agrego un boton de regreso al MainActivity y cambio el titulo del action bar
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         binding = ActivityScannerAddDeviceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 123)
         }else{
             startScanning()
@@ -50,8 +72,99 @@ class ScannerAddDeviceActivity : AppCompatActivity() {
 
         codeScanner.decodeCallback = DecodeCallback {
             runOnUiThread {
-                Toast.makeText(this, "Scan Result: ${it.text}", Toast.LENGTH_SHORT).show()
+
+                var list = it.text.split(" ")
+                if (list.size > 1) {
+
+                    val devId = list[0]
+                    val devCode = list[1]
+
+                    if (devId.isNotEmpty() && devCode.isNotEmpty()) {
+
+                        dbDevicesUsers.document(firebaseAuth.currentUser!!.email!!)
+                            .collection("linked_devices").document(devId)
+                            .get().addOnSuccessListener {
+                                if (it.data.isNullOrEmpty()) {
+                                    dbDevices.document(devId).get()
+                                        .addOnSuccessListener { document ->
+                                            if (document.data == null) {
+                                                Toast.makeText(
+                                                    this,
+                                                    "Couldn't find device",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                                val intent =
+                                                    Intent(this, DevicesActivity::class.java)
+                                                startActivity(intent)
+                                            } else {
+                                                if (document.data!!.containsValue(devCode)) {
+                                                    Toast.makeText(
+                                                        this,
+                                                        "Set up your new device!",
+                                                        Toast.LENGTH_SHORT
+                                                    ).show()
+                                                    val intent =
+                                                        Intent(this, AddDeviceActivity::class.java)
+                                                    val extras = Bundle()
+                                                    extras.putString("device", devId)
+                                                    extras.putString(
+                                                        "model",
+                                                        document.data!!["model"].toString()
+                                                    )
+                                                    intent.putExtra("extras", extras)
+                                                    startActivity(intent)
+                                                } else {
+                                                    Toast.makeText(
+                                                        this,
+                                                        "Couldn't find device",
+                                                        Toast.LENGTH_SHORT
+                                                    ).show()
+                                                    val intent =
+                                                        Intent(this, DevicesActivity::class.java)
+                                                    startActivity(intent)
+
+                                                }
+                                            }
+                                        }
+                                        .addOnFailureListener {
+                                            Toast.makeText(
+                                                this,
+                                                "Ups, something went wrong. Try again!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                            val intent = Intent(this, DevicesActivity::class.java)
+                                            startActivity(intent)
+                                        }
+
+                                } else {
+                                    Toast.makeText(
+                                        this,
+                                        "You already have this device!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val intent = Intent(this, DevicesActivity::class.java)
+                                    startActivity(intent)
+                                }
+                            }.addOnFailureListener { e ->
+                                Log.w(
+                                    ContentValues.TAG,
+                                    "Error Reading document",
+                                    e
+                                )
+                            }
+                    } else {
+                        Toast.makeText(this, "Couldn't find device", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, DevicesActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                }else{
+                    Toast.makeText(this, "Couldn't find device", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, DevicesActivity::class.java)
+                    startActivity(intent)
+                }
             }
+
         }
 
         codeScanner.errorCallback = ErrorCallback {
